63eb96ff7aff8425963b1dc3eb6e2babd80b5754
SAJEED006 <110912467+SAJEED006@users.noreply.github.com>
2022-10-02 12:22:37 +0530

    Add files via upload

---
+++ Hello_world_simple program.py
@@ -0,0 +1,24 @@
+"""  >>>>> MY FIRST HELLO WORLD PROGRAM AND EVERYBODY KNOWS PYTHON IS 
+INTERPRETED LANGUAGE (CODE IS EXECUTED AS SOON AS IT IS WRITTEN)"""
+""" >>> PROTOTYPING IS VERY QUICK  
+      OBJECT ORIENTED  WAY"""
+
+
+
+print("HELLO WORLD")
+
+"""  another program  for printing my friends names  [MAAZ][ADIL][AZEEM][MUDASSAR][AVINASH][VINIT]"""
+x="sajeed-----"
+print(x)
+y="maaz------"
+print(y)
+
+
+
+""" using one variable print all friends names"""
+z="adil----"
+k="avinash------"
+s="azeem---------"
+m="mudassar------"
+c="vinit------"
+print(x+y+z+k+s+m+c)
