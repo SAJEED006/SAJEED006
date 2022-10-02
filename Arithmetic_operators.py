e9a150f998a8d6e306709144c8c7ef72795a92c3
SAJEED006 <110912467+SAJEED006@users.noreply.github.com>
2022-10-02 10:54:11 +0530

    Add files via upload

---
+++ Arithmetic_operators.py
@@ -0,0 +1,12 @@
+"""my frist python program using arithmetic operatons
+ learn python with sajeed easy tutorials
+airthmetic operators are=[+,-,*,/,**,%,^]"""
+x=2
+y=4
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(x%y)
+print(x^y)
