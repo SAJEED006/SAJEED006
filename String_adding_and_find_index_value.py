4b076dad1d52f48ce47c5f561292bf7d5fa31ef4
SAJEED006 <110912467+SAJEED006@users.noreply.github.com>
2022-10-03 15:03:33 +0530

    Add files via upload

---
+++ String_adding_and_find_index_value.py
@@ -0,0 +1,10 @@
+#  THIS PROGRAM INDICATES ABOUT ADDING TWO STRINGS USING TWO VARIABLES
+x="SAJEED AND MAAZ ARE BOTH BROTHERS OF "
+y="ADIL"
+z=(x+y)
+print(z)
+
+
+# this program indicates finding  the index value storeS which letter in the string 
+# IN ABOVE CODE LINES X IS VARIABLE PRINT M LETTER USING INDEX VALUE
+print(x[11])
