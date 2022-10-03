1111cadf911efe4d848006d75197676bda1344a7
SAJEED006 <110912467+SAJEED006@users.noreply.github.com>
2022-10-03 14:51:44 +0530

    Add files via upload

---
+++ Type_casting.py
@@ -0,0 +1,15 @@
+"""TYPE CASTING IN PYTHON  """
+"""FIRTST CHECK  THE WHICH DATA TYPE IT IS ?"""
+x=2
+y=3.3
+z="sajeed is not programmer"
+b=2+4j
+print(type(x))
+print(type(y))
+print(type(z))              
+print(type(b))
+""" convert above data types"""
+print(int(y))
+print(float(x))
+# we cannot convert string into float and integer typer because gives erroe like>>>ValueError: invalid literal for int() with base 10: 'sajeed is not programmer'
+# ok ok ! i end this program with my type casting...............
