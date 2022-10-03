b226735f00041d7cfd9e56462f2ba5112888e653
SAJEED006 <110912467+SAJEED006@users.noreply.github.com>
2022-10-03 16:03:43 +0530

    Add files via upload

---
+++ different_functions.py
@@ -0,0 +1,37 @@
+
+
+
+x="                     sajeed is a UI/UX designer"         
+print(x.strip())                                            #it removes space before string strip() function is used
+
+x="                              sajeed is ui/ux designer "
+print(x)                                                         #without strip function output is different
+
+
+
+
+
+z="maaz and adil are both brothers "
+print(z.replace("adil","sajeed"))                                    #replace function
+
+k="hyderabad ,briyani, is ,very, delicious "
+print(k.split(","))                                                  #split function 
+
+
+s="python is very"
+c="easy"
+print(s+c)
+
+
+w="welcome to my  reception on {}th date  "
+t=16
+print(w.format(t))                                       #inserting a number in string 
+
+
+
+p="sajeed"
+print(len(p))                                     #find  the length of string using length function
+
+
+print(p.upper())                              # capital letters 
+
