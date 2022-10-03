5071af2064b59d3f2099492c74c0f451f855d2c7
SAJEED006 <110912467+SAJEED006@users.noreply.github.com>
2022-10-03 15:25:52 +0530

    Add files via upload

---
+++ Different_slicing_methods.py
@@ -0,0 +1,9 @@
+x="this is my book "                      #SLICING 
+print(x[8:10])                   
+
+
+y="Romeo and Juliet"                    #SLICE TO THE END 
+print(y[0:])
+
+z="Murgi kya jaane ande ka kya hoga"
+print(z[-33:-2])                           #NEGATIVE SLICING 
