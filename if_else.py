5f3162bd91554c2de0e566bc5a177c0a14622fe1
SAJEED006 <110912467+SAJEED006@users.noreply.github.com>
2022-10-10 20:10:08 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,8 @@
+name=["maaz","sajeed","adil","sachin","vinit","avinash","mudassar","azeem"]
+print(name)
+name=input("enter the name=")
+if name=="avinash":
+    print("yes true he is my brother")
+else:
+	print("they are not my brothers") 
+	
