6f61af4c808422d4ecd1135c5e6c4cb7cf3875f3
SAJEED MALAGI <110912467+SAJEED006@users.noreply.github.com>
2022-10-25 22:47:43 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,16 @@
+#array is also like list
+#alredy we learnt  about the list now it easy to understand the array
+# in realWORLD  we call someone "ARRAY BHAI MAAZ TU ZALDI KYU NAI ATTHA COLLEGE KO"
+#IN PROGRAMMING LANGUAGE ARRAY IS ONE DATA TYPE 
+# LET US START>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+n=[1,2,3,4,5,6]
+print(n)                 #printing all values
+print(len(n))               #printing how many values in the array
+print(n[0])                  #printing first value using 0 index
+print(len(n)-1)             #printing last value using (n-1) concept
+n[2]=4                         #replacing the value inthe array
+print(n)
+n.append(7)                 #adding the value to array 
+print(n)
+n.pop(5)                   #removing the value in the array
+print(n)
