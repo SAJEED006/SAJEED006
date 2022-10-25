cf4d2ce50e503142a8a695512700d281f785670d
SAJEED MALAGI <110912467+SAJEED006@users.noreply.github.com>
2022-10-25 17:55:22 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,21 @@
+
+a=20
+b=10
+def addition():                #different functions [+,-,*,/]
+    print(a+b)
+def subtraction():
+    print(a-b)
+def multiply():
+    print(a*b)
+def division():
+    print
+    print(a/b)
+def modulus():
+    print(a%b)
+
+
+addition()
+subtraction()  #we can give values as parameters that is also possible 
+multiply()
+division()
+modulus()
