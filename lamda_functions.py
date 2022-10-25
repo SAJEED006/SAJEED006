b3919bfba36b5b9a44eb8218029751a3e8896c90
SAJEED MALAGI <110912467+SAJEED006@users.noreply.github.com>
2022-10-25 18:04:04 +0530

    Add files via upload

---
+++ lamda_functions.py
@@ -0,0 +1,11 @@
+# this program indicates lamda functions 
+#lamda functions can take any number of arguments 
+#python program>>>>>>>>>>>>>>>>>>>
+from calendar import c
+from msvcrt import kbhit
+
+
+v=lambda a,b,c,d,e,f,g,h:a+b+c+d+e+f+g+h
+print(v(1,1,1,1,1,1,1,1))
+
+
